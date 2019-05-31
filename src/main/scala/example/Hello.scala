44fdf4b320b8a59f213f251284009b317c648b6d
Torkel Rogstad <torkel@rogstad.io>
2019-05-31 15:04:08 +0200

    Validate address

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -1,8 +1,14 @@
 package example
 
 import org.bitcoins.rpc.serializers.JsonSerializers
+import org.bitcoins.rpc.serializers.JsonSerializers._
+import play.api.libs.json._
+import org.bitcoins.core.protocol.BitcoinAddress
 
 object Hello extends App {
   println(JsonSerializers)
   println(JsonSerializers.addressInfoResultReads)
+  println(
+    JsString("3J98t1WpEZ73CNmQviecrnyiWrnqRhWNLy").validate[BitcoinAddress]
+  )
 }
